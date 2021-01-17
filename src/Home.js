d226c761786a4cfb8e5e4be52c45c5a488c30191
Atharva2018 <atharva.shirode99@gmail.com>
2021-01-17 18:39:05 +0530

    Added Header/Navbar.

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <img className="home_img" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB28684220_.jpg"/>
+        </div>
+    )
+}
+
+export default Home
