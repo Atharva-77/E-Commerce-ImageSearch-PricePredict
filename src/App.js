99b065e094f8bdbdc60826029c24577aaa6530cf
Atharva-77 <atharva.shirode99@gmail.com>
2022-08-02 11:49:10 +0530

    In App.js, ProductDetails.js, Store, Login_brad added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import Checkout from './Checkout';
 import Register from './Register';
 import Login from './Login';
 import Seller from './Seller'
+import ProductDetails from './ProductDetails'
+
+import {Provider} from 'react-redux'
+import store from './Store'
+import Login_brad from './Login_brad';
 
 function App() {
   return (
+<Provider store={store} >
     <div className="app">
       <Router >
          
@@ -22,23 +28,37 @@ function App() {
                   <Login />
               </Route>
 
+              <Route path="/login_brad">
+                  <Login_brad />
+              </Route>
+
               <Route path="/seller">
                   <Seller />
                </Route>
 
-              <Route path="/checkout"> 
+              <Route path="/checkout/:id?"> 
+              {/* or <Route path="/checkout/">  */}
                  <Header />        
                  <Checkout />
+              </Route>
+
+
+              <Route path="/product/:id"> 
+                 <Header />        
+                 <ProductDetails />
               </Route>
 
               <Route path="/">
                 <Header />
                 <Home />
               </Route>
+
           </Switch>
       </Router>
       
     </div>
+    </Provider>
+
   );
 }
 
